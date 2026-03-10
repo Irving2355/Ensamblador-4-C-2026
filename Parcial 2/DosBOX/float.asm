79fc727fa5dcd680207f0fa687a489356cc93a7b
Irving2355 <it235597@gmail.com>
2026-03-10 08:41:29 -0600

    Clase 10-03-2026

---
+++ Parcial 2/DosBOX/float.asm
@@ -0,0 +1,67 @@
+;punto flotante 
+
+.model small
+.stack 100h
+
+.data
+A_int dw 5
+A_frac dw 50
+B_int dw 2
+B_frac dw 25
+
+;float 32-bit (REAL4)
+C3 dd 3.0
+C4 dd 4.0
+T1 dd 0.10
+T2 dd 0.20
+
+A_val dd ?
+B_val dd ?
+sumaAB dd ?
+prodAB dd ?
+divAB dd ?
+sqrtD dd ?
+exprZ dd ?
+testT dd ?
+tmpInt dw 0
+tmpFrac dw 0
+HUNDRED dw 100
+dataSeg dw 0 
+
+.code
+
+make_real_from_parts proc
+    fild tmpInt   ;sto = int
+    fild tmpFrac  ; fracc, st1= int
+    fidiv HUNDRED ; fracc/100 
+    fadd          ; sto = int + (fracc/100) 5 + .50 
+    ret 
+make_real_from_parts endp
+main:
+
+    mov ax, @data
+    mov ds, ax
+
+    ; construir A_val
+    mov ax, A_int
+    mov tmpInt, ax
+    mov ax, A_frac
+    mov tmpFrac, ax 
+    call make_real_from_parts 
+    fstp A_val
+
+    ; construir B_val
+    mov ax, B_int
+    mov tmpInt, ax
+    mov ax, B_frac
+    mov tmpFrac, ax 
+    call make_real_from_parts 
+    fstp B_val
+
+    ;******** suma ***********
+    fld A_val ; sto = A_val
+    fld B_val ; sto = B_val, A_val
+    fadd 
+    fstp sumaAB
+
+end main
