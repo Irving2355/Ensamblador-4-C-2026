248af2db7af016fbc2b035c4585cb8d185368188
Irving2355 <it235597@gmail.com>
2026-05-07 08:56:11 -0600

    Clase 07-05-2026

--- Parcial 3/compilar.bat
+++ Parcial 3/compilar.bat
@@ -1,21 +1,29 @@
 @echo off
-setlocal enabledeleyedexpansion
+setlocal enabledelayedexpansion
 
 REM comentario
 :: comentario
 
 for %%I in (.) do set "CARPETA=%%~nxI"
 
-echo 
-echo Compilando proyecto ...
-echo 
+echo .
+echo Compilando el proyecto
+echo .
 
 for %%F in (*.asm) do (
+    echo Compilando %%F ...
+    echo .
     nasm -f win64 "%%F" -o "%%~nF.obj"
     if errorlevel 1 (
-        echo Error al compilar nasm
+        echo Error al compilar Nasm 
         exit /b 1
     )
+)
+
+::guardemos en vectores los .obj y los .c
+set "OBJS="
+for %%F in (*.obj) do (
+    set "OBJS=!OBJS! %%F"
 )
 
 set "CSRC="
@@ -23,16 +31,17 @@ for %%F in (*.c) do (
     set "CSRC=!CSRC! %%F"
 )
 
-set "OBJS="
-for %%F in (*.obj) do (
-    set "OBJS=!OBJS! %%F"
-)
+::enlazar en gcc
+echo .
+echo Compilando en GCC 
 
 gcc %CSRC% %OBJS% -o "%CARPETA%.exe"
 if errorlevel 1 (
-    echo Error el compilar gcc 
+    echo Error al compilar GCC 
     exit /b 1
-) 
+)
 
+echo.
+echo Finalizado el compilado
 endlocal
 pause
